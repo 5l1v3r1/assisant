80307ca0264c4bb4b5cd99f5dbd5a75a43de0e27
repoog <repoog@gmail.com>
2019-01-19 18:49:56 +0800

    add: add readme file and sql file

---
+++ assit.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MariaDB
+ Source Server Version : 100136
+ Source Host           : localhost:3306
+ Source Schema         : assit
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100136
+ File Encoding         : 65001
+
+ Date: 19/01/2019 18:49:08
+*/
+
